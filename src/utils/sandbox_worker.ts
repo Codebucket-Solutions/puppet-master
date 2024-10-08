da40c0a3b839420247a6a5415766bc51420d1f0b
Abhinav Gautam <abhinavgautam@Abhinavs-MacBook-Pro.local>
2024-10-08 21:27:31 +0530

    changes

--- src/utils/sandbox_worker.ts
+++ src/utils/sandbox_worker.ts
@@ -4,10 +4,10 @@ import path from "path";
 import { v4 } from "uuid";
 
 module.exports = async (job: SandboxedJob) => {
-
+    console.log("Sandboxed Worker Started", job.id);
     const payload = job.data;
 
-    const browser = await puppeteer.launch(payload.options);
+    const browser = await puppeteer.launch(payload.launchOptions);
     const page = await browser.newPage();
     if(payload.content)
         await page.setContent(payload.content, payload.pageOptions);
